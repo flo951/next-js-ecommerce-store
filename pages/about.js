ed3673be918505accee5870e55018c66ad0fab55
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-03 20:36:08 +0100

    Add Layout component and code along video from karl

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,16 @@
-import Header from '../components/Header';
+import Head from 'next/head';
+import Layout from '../components/Layout';
 
 export default function About() {
   return (
-    <>
-      <Header />
-      <main>
-        <h1>About Page</h1>
-        <p>This is the about page</p>
-      </main>
-    </>
+    <Layout>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="About the team" />
+      </Head>
+
+      <h1>About Page</h1>
+      <p>This is the about page</p>
+    </Layout>
   );
 }
