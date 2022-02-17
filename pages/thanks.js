5295853c49caa62791420f88033ac7166aa61b0d
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-17 09:31:50 +0100

    Add serversideprops to have cookie data on page

--- pages/thanks.js
+++ pages/thanks.js
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import { getPokemons } from '../util/database';
 
 const headingStyles = css`
   color: white;
@@ -11,15 +12,35 @@ const centerHeadingStyles = css`
   justify-content: center;
 `;
 
-export default function Thanks() {
+export default function Thanks(props) {
+  console.log(props);
   return (
-    <Layout>
+    <>
       <Head>
         <title>Thank you for your order</title>
       </Head>
       <div css={centerHeadingStyles}>
         <h1 css={headingStyles}>Thank you for your purchase</h1>
       </div>
-    </Layout>
+    </>
   );
+}
+
+export async function getServerSideProps(context) {
+  // context allow to acces cookies
+  // important, always return an object from getserversideprops and always return a key (props is the key)
+
+  const likedPokemonsOnCookies = context.req.cookies.likedPokemons || '[]';
+
+  const likedPokemons = JSON.parse(likedPokemonsOnCookies);
+  // // 1. get the cookies from the browser
+
+  // 2. pass the cookies to the frontend
+  const pokemonsInDb = await getPokemons();
+  return {
+    props: {
+      likedPokemons: likedPokemons,
+      pokemonsInDb,
+    },
+  };
 }
