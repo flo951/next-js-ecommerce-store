67d8911a391938b330038b08020ed83e08ec5b40
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-17 19:39:43 +0100

    Write unit tests with jest for cookie data

---
+++ pages/products/__tests__/cookies.test.js
@@ -0,0 +1,35 @@
+import {
+  handleAddToCookie,
+  updateAmount,
+  getAmount,
+  handleDeleteCookie,
+} from '../cookieFunctions.js';
+
+const pokemons = [
+  { id: 1, amount: 2, name: 'Glumanda' },
+  { id: 2, amount: 2, name: 'Bisasam' },
+];
+
+test('sum pokemons in cart', () => {
+  expect(getAmount(pokemons)).toBe(4);
+});
+
+test('update amount of in cookie when adding same product', () => {
+  const updatePokemons = [
+    { id: 2, amount: 2, name: 'Bisasam' },
+    { id: 1, amount: 4, name: 'Glumanda' },
+  ];
+  expect(updateAmount(1, 2, pokemons)).toStrictEqual(updatePokemons);
+});
+
+test('delete pokemon with id 1', () => {
+  const deletePokemons = [{ id: 2, amount: 2, name: 'Bisasam' }];
+  expect(handleDeleteCookie(1, pokemons)).toStrictEqual(deletePokemons);
+});
+
+test('add pokemon with id 1', () => {
+  const deletePokemons = [{ id: 1, amount: 2, name: 'Glumanda' }];
+  expect(handleAddToCookie(2, 2, 'Bisasam', deletePokemons)).toStrictEqual(
+    pokemons,
+  );
+});
