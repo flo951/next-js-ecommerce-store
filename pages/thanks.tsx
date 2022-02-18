68e0e7957647e5c1928bf8f308496230c959952d
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-18 14:58:19 +0100

    Convert page to typescript

---
+++ pages/thanks.tsx
@@ -0,0 +1,41 @@
+import { css } from '@emotion/react';
+import { GetServerSidePropsContext } from 'next';
+import Head from 'next/head';
+import Layout from '../components/Layout';
+import { getPokemons, getSinglePokemon } from '../util/database';
+
+const headingStyles = css`
+  color: white;
+`;
+
+const centerHeadingStyles = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export default function Thanks() {
+  return (
+    <div>
+      <Head>
+        <title>Thank you for your order</title>
+      </Head>
+      <div css={centerHeadingStyles}>
+        <h1 css={headingStyles}>Thank you for your purchase</h1>
+      </div>
+    </div>
+  );
+}
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const likedPokemonsOnCookies = context.req.cookies.likedPokemons || '[]';
+
+  const likedPokemons = JSON.parse(likedPokemonsOnCookies);
+
+  // Only get single pokemon with id from db and store it in variable pokemon
+  const pokemons = await getPokemons();
+  return {
+    props: {
+      likedPokemons: likedPokemons,
+      pokemons: pokemons,
+    },
+  };
+}
