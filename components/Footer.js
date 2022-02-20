6712c814ce3387b75a45f2209bba83a1c89772d3
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-20 19:52:47 +0100

    Test comment out footer, maybe delete

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,29 @@
-import Link from 'next/link';
-import { css } from '@emotion/react';
+// import Link from 'next/link';
+// import { css } from '@emotion/react';
 
-const footerStyles = css`
-  padding: 20px 20px;
-  margin: 1rem 1rem 1rem 1rem;
-  border-radius: 8px;
-  background-color: #01397a;
-  a {
-    color: white;
-    text-decoration: none;
-    margin: 5px;
-    font-size: 32px;
-  }
-`;
+// const footerStyles = css`
+//   padding: 20px 20px;
+//   margin: 1rem;
+//   border-radius: 8px;
 
-export default function Footer() {
-  return (
-    <header css={footerStyles}>
-      <Link href="/">
-        <a>About</a>
-      </Link>
-      <Link href="/about">
-        <a>Contact</a>
-      </Link>
-    </header>
-  );
-}
+//   background-color: #01397a;
+//   a {
+//     color: white;
+//     text-decoration: none;
+//     margin: 5px;
+//     font-size: 32px;
+//   }
+// `;
+
+// export default function Footer() {
+//   return (
+//     <footer css={footerStyles}>
+//       <Link href="/">
+//         <a>About</a>
+//       </Link>
+//       <Link href="/about">
+//         <a>Contact</a>
+//       </Link>
+//     </footer>
+//   );
+// }
