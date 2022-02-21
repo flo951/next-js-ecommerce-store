fb44ed28b2a50265118f8e84bebe950d5522e731
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-21 11:07:37 +0100

    Add github workflows

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,10 +8,10 @@
 // export default config;
 
 const config = {
-  //testEnvironment: 'jsdom',
-  //testPathIgnorePatterns: ['<rootDir>/integration'],
-  preset: 'jest-puppeteer',
-  //testPathIgnorePatterns: ['<rootDir>/pages/products/__tests__'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/integration'],
+  // preset: 'jest-puppeteer',
+  // testPathIgnorePatterns: ['<rootDir>/pages/products/__tests__'],
 };
 
 export default config;
