7698d2a43c5b78a29f0158ef054befbe2f5b8059
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-21 10:49:09 +0100

    Add new config for tests

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   launch: {
-    // Comment this in if you want to see the browser
     headless: false,
   },
   browserContext: 'default',
+  server: {
+    command: 'yarn dev',
+  },
 };
