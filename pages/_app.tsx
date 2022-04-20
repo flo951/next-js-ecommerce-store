89e538ed419cb56633a641529848d2e5e5dad1a0
Florian Görlich <florian.goerlich95@gmail.com>
2022-04-20 08:15:56 +0200

    Add function to store previous page in session storage, so after login the user is redirected back to the page where he was

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,47 @@
 import { css, Global } from '@emotion/react';
 import { AppProps } from 'next/app';
-
+import { useCallback, useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [user, setUser] = useState();
+  const [amountInCart, setAmountInCart] = useState<number>();
+  const refreshUserProfile = useCallback(async () => {
+    const response = await fetch('/api/profile');
+    const data = await response.json();
+
+    if ('errors' in data) {
+      console.log(data.errors);
+      setUser(undefined);
+      return;
+    }
+
+    setUser(data.user);
+  }, []);
+
+  useEffect(() => {
+    refreshUserProfile().catch(() => {});
+  }, [refreshUserProfile]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    storePathValues();
+  }, [router.asPath]);
+
+  function storePathValues() {
+    const storage = globalThis.sessionStorage;
+
+    // Set the previous path as the value of the current path.
+    const prevPath = storage.getItem('currentPath');
+    if (!prevPath) {
+      return;
+    }
+    storage.setItem('prevPath', prevPath);
+    // Set the current path value by looking at the browser's location object.
+    storage.setItem('currentPath', globalThis.location.pathname);
+  }
+
   return (
     <>
       <Global
@@ -18,8 +58,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           }
         `}
       />
-
-      <Component {...pageProps} />
+      <Layout userObject={user} amountInCart={amountInCart}>
+        <Component
+          refreshUserProfile={refreshUserProfile}
+          {...pageProps}
+          setAmountInCart={setAmountInCart}
+          amountInCart={amountInCart}
+        />
+      </Layout>
     </>
   );
 }
